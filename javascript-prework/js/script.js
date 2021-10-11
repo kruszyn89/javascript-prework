2add82251abbf7462577cf6b56a2c8c050bda0e8
Dawid Kruszyński <dawid.kruszynski89@gmail.com>
2021-10-11 22:50:31 +0200

    added game logic

--- javascript-prework/js/script.js
+++ javascript-prework/js/script.js
@@ -1,27 +1,4 @@
-// let computerMove = 'kamień';
-
-// printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-// let playerMove = `papier`;
-
-// printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-// let player2Move = `nozyce`;
-
-// printMessage('Zagrałem ' + player2Move + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-
-// let randomFraction = Math.random();
-
-// printMessage('Wylosowany ułamek to: ' + randomFraction);
-
-// let calculation = randomFraction * 3 + 1;
-
-// printMessage('Ułamek pomnożony przez 3 i powiększony o 1: ' + calculation);
-
-// let roundNumber = Math.floor(calculation);
-
-// printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
+// Ruch komputera
 
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 printMessage('Liczba po zaokrągleniu w dół to: ' + randomNumber);
@@ -30,11 +7,18 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
-if(randomNumber == 1){
-  computerMove = 'kamień';
+if( randomNumber == 1) {
+    computerMove = 'kamień';
+} else if ( randomNumber == 2) {
+    computerMove = 'papier';
+} else {
+    computerMove = 'nożyce'
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+
+// Ruch gracza 
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
@@ -42,8 +26,41 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
-  playerMove = 'kamień';
+if( playerInput == '1') {
+    playerMove = 'kamień';
+} else if ( playerInput == '2') {
+    playerMove = 'papier';
+} else if ( playerInput == '3') {
+    playerMove = 'nożyce';
+} else if ( playerInput == '') {
+    playerMove = 'Nie wybrano zadnej opcji, spróbuj jeszcze raz!'
+}
+  else {
+    playerMove = 'Wybierz cyfrę z przedzialu od 1 do 3'
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+
+// Wynik gry
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+  else if (computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('Komputer wygrywa!');
+} else if (computerMove == 'nożyce' && playerMove == 'papier') {
+    printMessage('Komputer wygrywa!');    
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('Komputer wygrywa!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
+    printMessage('Remis!');
+} else if (computerMove == 'papier' && playerMove == 'papier') {
+    printMessage('Remis!');
+} else if (computerMove == 'kamień' && playerMove == 'kamień') {
+    printMessage('Remis!');
+}
