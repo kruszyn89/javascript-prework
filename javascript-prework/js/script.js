650fe09ea3dca0785054f25536f8bb3b5f44224f
Dawid Kruszyński <dawid.kruszynski89@gmail.com>
2021-10-13 16:36:00 +0200

    trying to add buttons

--- javascript-prework/js/script.js
+++ javascript-prework/js/script.js
@@ -1,36 +1,41 @@
-function getMoveName(randomNumber){
-    if(randomNumber == 1) {
-        return 'kamień';
-    } else if ( randomNumber == 2) {
-        return 'papier';
-    } else if ( randomNumber == 3) {
-        return 'nożyce';
-    } else { 
-        return 'Proszę wybrać poprawny numer!'
+function playGame(){
+
+    function getMoveName(randomNumber){
+        if(randomNumber == 1) {
+            return 'kamień';
+        } else if ( randomNumber == 2) {
+            return 'papier';
+        } else if ( randomNumber == 3) {
+            return 'nożyce';
+        } else { 
+            return 'Proszę wybrać poprawny numer!'
+        }
     }
-}
-function displayResult(argComputerMove, argPlayerMove) {
-    if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
-        return('Ty wygrywasz!');
-      }
-      else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
-        return('Komputer wygrywa!');
-    } else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
-        return('Komputer wygrywa!');    
-    } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
-        return('Ty wygrywasz!');
-    } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
-        return('Komputer wygrywa!');
-    } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
-        return('Ty wygrywasz!');
-    } else if (argComputerMove == 'nożyce' && argPlayerMove == 'nożyce') {
-        return('Remis!');
-    } else if (argComputerMove == 'papier' && argPlayerMove == 'papier') {
-        return('Remis!');
-    } else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień') {
-        return('Remis!');
+    function displayResult(argComputerMove, argPlayerMove) {
+        if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+            return('Ty wygrywasz!');
+        }
+        else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
+            return('Komputer wygrywa!');
+        } else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
+            return('Komputer wygrywa!');    
+        } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+            return('Ty wygrywasz!');
+        } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
+            return('Komputer wygrywa!');
+        } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+            return('Ty wygrywasz!');
+        } else if (argComputerMove == 'nożyce' && argPlayerMove == 'nożyce') {
+            return('Remis!');
+        } else if (argComputerMove == 'papier' && argPlayerMove == 'papier') {
+            return('Remis!');
+        } else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień') {
+            return('Remis!');
+        } else 
+            return ('Gracz nie wybrał zadnej opcji, proszę spróbuj ponownie!');
+          
     }
-}
+
 
     let randomNumber = Math.floor(Math.random() * 3 + 1);
     printMessage('Liczba po zaokrągleniu w dół to: ' + randomNumber);
@@ -48,11 +53,9 @@ function displayResult(argComputerMove, argPlayerMove) {
     let final = displayResult(argComputerMove, argPlayerMove);
     console.log('moves:', argComputerMove, argPlayerMove);
     printMessage('Wynik to: ' + final);
+}
 
-
-
-
-
+playGame(3);
 
 
 
